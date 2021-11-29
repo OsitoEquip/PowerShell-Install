1b067f5c6a078a14fb591cf59e54a2f9978d64c8
Rick Casady <richard.casady@gmail.com>
2021-11-28 21:33:32 -0600

    Update FunLib.ps1

--- src/FunLib.ps1
+++ src/FunLib.ps1
@@ -566,3 +566,19 @@ function global:ExecuteCert($certs)
 	}
 }
 
+
+# --------------------- Run as -----------------
+
+function Start-NewProcess(
+        [string]$pfile,
+        [string]$arguments
+    ){
+    $p = New-Object System.Diagnostics.Process;
+    $p.StartInfo.UseShellExecute = $false;
+    $p.StartInfo.RedirectStandardOutput = $true;
+    $p.StartInfo.FileName = $pfile;
+    $p.StartInfo.Arguments = $arguments
+    [void]$p.Start();
+    $p.WaitForExit();
+    $p.StandardOutput.ReadToEnd();
+}
